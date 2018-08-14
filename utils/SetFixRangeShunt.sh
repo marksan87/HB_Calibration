a89b21d11a9f6ba08344564c022532e1dffb7139
Mark Saunders <msaunder2004@my.fit.edu>
2018-08-14 12:16:21 -0500

    first test of implementation of range transition scan, DID NOT WORK

--- utils/SetFixRangeShunt.sh
+++ utils/SetFixRangeShunt.sh
@@ -3,16 +3,25 @@
 range=$1
 shunt=$2
 
+if [ $range -lt 4 ]
+then
+    (>&2 echo "    Starting range $range shunt $shunt")
 
-(>&2 echo "    Starting range $range shunt $shunt")
+    echo "put HB2-[3-4]-QIE[1-64]_FixRange 128*1" > serverCommands.txt
+    echo "put HB2-[3-4]-QIE[1-64]_RangeSet 128*$range" >> serverCommands.txt
+    echo "put HB2-[3-4]-QIE[1-64]_Gsel 128*$shunt" >> serverCommands.txt
+    echo "wait 200" >> serverCommands.txt
+    echo "quit" >> serverCommands.txt
+else
+    (>&2 echo "    Starting range $range shunt $shunt")
 
+    echo "put HB2-[3-4]-QIE[1-64]_FixRange 128*0" > serverCommands.txt
+    echo "put HB2-[3-4]-QIE[1-64]_Gsel 128*$shunt" >> serverCommands.txt
+    echo "wait 200" >> serverCommands.txt
+    echo "quit" >> serverCommands.txt
+fi
 
-echo "put HB2-[3-4]-QIE[1-64]_FixRange 128*1" > serverCommands.txt
-echo "put HB2-[3-4]-QIE[1-64]_RangeSet 128*$range" >> serverCommands.txt
-echo "put HB2-[3-4]-QIE[1-64]_Gsel 128*$shunt" >> serverCommands.txt
-echo "wait 200" >> serverCommands.txt
-echo "quit" >> serverCommands.txt
-
+echo "HERE"
 cat serverCommands.txt
-
+ech "HERE"
 ngFEC.exe -p 64000 -H cmsnghcal01.fnal.gov -z < serverCommands.txt
