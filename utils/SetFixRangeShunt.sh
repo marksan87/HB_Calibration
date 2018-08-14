4c8cb670274e3d49789e88a4aa8e569a99656215
Mark Saunders <msaunder2004@my.fit.edu>
2018-08-14 15:31:09 -0500

    Fix the scans for the range transition, which are now listed as gsel 32

--- utils/SetFixRangeShunt.sh
+++ utils/SetFixRangeShunt.sh
@@ -3,19 +3,19 @@
 range=$1
 shunt=$2
 
-if [ $range -lt 4 ]
+if [ $shunt -eq 32 ]
 then
     (>&2 echo "    Starting range $range shunt $shunt")
 
-    echo "put HB2-[3-4]-QIE[1-64]_FixRange 128*1" > serverCommands.txt
-    echo "put HB2-[3-4]-QIE[1-64]_RangeSet 128*$range" >> serverCommands.txt
-    echo "put HB2-[3-4]-QIE[1-64]_Gsel 128*$shunt" >> serverCommands.txt
+    echo "put HB2-[3-4]-QIE[1-64]_FixRange 128*0" > serverCommands.txt
+    echo "put HB2-[3-4]-QIE[1-64]_Gsel 128*0" >> serverCommands.txt
     echo "wait 200" >> serverCommands.txt
     echo "quit" >> serverCommands.txt
 else
     (>&2 echo "    Starting range $range shunt $shunt")
 
-    echo "put HB2-[3-4]-QIE[1-64]_FixRange 128*0" > serverCommands.txt
+    echo "put HB2-[3-4]-QIE[1-64]_FixRange 128*1" > serverCommands.txt
+    echo "put HB2-[3-4]-QIE[1-64]_RangeSet 128*$range" >> serverCommands.txt
     echo "put HB2-[3-4]-QIE[1-64]_Gsel 128*$shunt" >> serverCommands.txt
     echo "wait 200" >> serverCommands.txt
     echo "quit" >> serverCommands.txt
